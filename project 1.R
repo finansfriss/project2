7502b6cfa071864a10341ffc45b3ee49d301e0ad
erikfjonsson <erik.jonsson@live.com>
2018-02-22 11:04:29 +0100

    Update project 1.R

--- project 1.R
+++ project 1.R
@@ -8,15 +8,15 @@ setwd("C:/Users/erikj/Documents/764 local files/project 1") #set working directo
 
 library(RSQLite) #get the RSQLite library
 
-loansacc = read.csv("loans_accepted.csv", header=TRUE, sep=",", dec="." ) #import data with accepted loans
-loansrej = read.csv("loans_rejected.csv", header=TRUE, sep=",", dec="." ) #import data with rejected loans
+loansacc_original = read.csv("loans_accepted.csv", header=TRUE, sep=",", dec="." ) #import data with accepted loans
+loansrej_original = read.csv("loans_rejected.csv", header=TRUE, sep=",", dec="." ) #import data with rejected loans
 
 
 ## create database
 
 db = dbConnect(SQLite(), dbname="loans.sqlite") #create the database
 
-dbWriteTable(conn = db, name = "loansacc", loansacc, overwrite=T, row.names=FALSE) #create table for accepted loans
-dbWriteTable(conn = db, name = "loansrej", loansrej, overwrite=T, row.names=FALSE) #create table for rejected loans
+dbWriteTable(conn = db, name = "loansacc_original", loansacc_original, overwrite=T, row.names=FALSE) #create table for accepted loans
+dbWriteTable(conn = db, name = "loansrej_original", loansrej_original, overwrite=T, row.names=FALSE) #create table for rejected loans
 
 dbDisconnect(db) #disconnect
